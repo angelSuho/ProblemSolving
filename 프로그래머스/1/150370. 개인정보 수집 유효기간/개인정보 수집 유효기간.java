d5d18ab5899013720f279caf19b9e8a2cf18526f
SoohoLee <agsooho0303@gmail.com>
2023-11-14 23:40:20 +0900

    Update 개인정보 수집 유효기간.java

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -10,18 +10,20 @@ class Solution {
         }
 
         LocalDate todayDate = LocalDate.parse(today.replace(".", "-"));
-        List<Integer> toBeDisposed = new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
 
         for (int i = 0; i < privacies.length; i++) {
             String[] privacyParts = privacies[i].split(" ");
+            
             LocalDate collectedDate = LocalDate.parse(privacyParts[0].replace(".", "-"));
             int monthsToAdd = termMap.get(privacyParts[1]);
-            LocalDate disposalDate = collectedDate.plusMonths(monthsToAdd);
-            if (disposalDate.isBefore(todayDate) || disposalDate.isEqual(todayDate)) {
-                toBeDisposed.add(i + 1);
+            
+            LocalDate calculatedDate = collectedDate.plusMonths(monthsToAdd);
+            if (calculatedDate.isBefore(todayDate) || calculatedDate.isEqual(todayDate)) {
+                result.add(i + 1);
             }
         }
 
-        return toBeDisposed;
+        return result;
     }
 }
