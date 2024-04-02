d40c20bfe9bfbbb11160d8645acf4657bb6cb337
Suho Lee <agsooho0303@gmail.com>
2024-04-02 19:33:07 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 7.87 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
@@ -7,17 +7,19 @@ class Solution {
 
     public int solution(int[][] board) {
         N = board.length;
-        return buildTrack(board);
+        return buildTrackByBfs(board);
     }
 
-    private int buildTrack(int[][] board) {
+    private int buildTrackByBfs(int[][] board) {
         Queue<int[]> queue = new LinkedList<>();
+        // x,y 위치, 방향, 누적비용
         queue.offer(new int[]{0, 0, -1, 0});
-        int[][][] costs = new int[N][N][4];
+        int[][][] accumCost = new int[N][N][4];
 
         while (!queue.isEmpty()) {
-            int[] cur = queue.poll();
-            int x = cur[0], y = cur[1], dir = cur[2], cost = cur[3];
+            int[] tmp = queue.poll();
+            int x = tmp[0], y = tmp[1];
+            int direction = tmp[2], cost =tmp[3];
 
             for (int i = 0; i < 4; i++) {
                 int nx = x + dx[i];
@@ -25,31 +27,38 @@ class Solution {
 
                 if (nx >= 0 && ny >= 0 && nx < N && ny < N && board[nx][ny] == 0) {
                     int newCost = cost;
-                    if (dir == -1 || dir == i) {
+                    if (direction == -1 || direction == i) {
                         // 직선 방향으로 이동하는 경우
                         newCost += 100;
                     } else {
-                        // 코너를 도는 경우
-                        // 코너 비용 + 직선 비용
+                        // 코너를 도는 경우, 코너 비용 + 직선 비용
                         newCost += 600;
                     }
 
                     // bfs로 갱신된 최소 비용 저장
-                    if (costs[nx][ny][i] == 0 || costs[nx][ny][i] > newCost) {
-                        costs[nx][ny][i] = newCost;
+                    if (accumCost[nx][ny][i] == 0 || accumCost[nx][ny][i] > newCost) {
+                        accumCost[nx][ny][i] = newCost;
                         queue.offer(new int[]{nx, ny, i, newCost});
                     }
                 }
             }
         }
-
-        // 최종 도착지점의 최소 비용 반환
-        int minCost = Integer.MAX_VALUE;
-        for (int i = 0; i < 4; i++) { // 상하좌우
-            // 방향마다의 최소 비용 중 최소값을 찾는다.
-            minCost = costs[N-1][N-1][i] != 0 ? Math.min(minCost, costs[N-1][N-1][i]) : minCost;
+        for (int i=0; i < accumCost.length; i++) {
+            for (int j=0; j< accumCost[i].length; j++) {
+                for (int k = 0; k<accumCost[i][j].length; k++) {
+                    System.out.print(accumCost[i][j][k] + " ");
+                }
+                System.out.println();
+            }
         }
 
-        return minCost;
+        // 최종 도착지점의 최소 비용 반환
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < 4; i++) {
+            // 방향마다의 최소 비용 중 최소값을 찾는다.
+            min = accumCost[N-1][N-1][i] != 0 ? Math.min(min, accumCost[N-1][N-1][i]) : min;
+        }
+
+        return min;
     }
 }
